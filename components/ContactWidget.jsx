13acbc9e38ac308652cc516e49754d73d529773f
sakhawat236hossain <hmdsakhawat236@gmail.com>
2026-04-22 05:42:12 +0600

    fixt error

--- components/ContactWidget.jsx
+++ components/ContactWidget.jsx
@@ -14,7 +14,7 @@ export default function ContactWidget() {
   href="https://wa.me/8801XXXXXXXXX"
   target="_blank"
   rel="noopener noreferrer"
-  className="w-14 h-14 bg-green-500 shadow-2xl rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform overflow-hidden"
+  className="w-14 h-14 shadow-2xl rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform overflow-hidden"
 >
   <img src="https://img.icons8.com/?size=100&id=A1JUR9NRH7sC&format=png&color=000000" alt="WhatsApp" className="w-8 h-8" />
 </a>
@@ -23,7 +23,7 @@ export default function ContactWidget() {
           {/* Phone Call */}
           <a
             href="tel:+8801851121472" 
-            className="w-14 h-14 cursor-pointer bg-gray-700 rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform text-white text-2xl"
+            className="w-14 h-14 cursor-pointer bg-gray-100 rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform text-white text-2xl"
           >
             <img src="https://img.icons8.com/?size=100&id=9729&format=png&color=000000" alt="Phone" className="w-8 h-8" />
           </a>
@@ -33,9 +33,9 @@ export default function ContactWidget() {
       {/* Main Toggle Button */}
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="w-16 h-16 cursor-pointer bg-gray-500 rounded-full flex items-center justify-center shadow-2xl hover:bg-gray-700 transition-all duration-300 text-white text-3xl"
+        className="w-16 h-16 cursor-pointer bg-gray-100 rounded-full flex items-center justify-center shadow-2xl hover:bg-gray-200 transition-all duration-300 text-white text-3xl"
       >
-        {isOpen ? <img src="https://img.icons8.com/?size=100&id=9433&format=png&color=000000" alt="Close" className="w-8 h-8" /> : <img src="https://img.icons8.com/?size=100&id=hByk2bcP4aZ5&format=png&color=000000" alt="Contact" className="w-8 h-8" />}
+        {isOpen ? <img  src="https://img.icons8.com/?size=100&id=11997&format=png&color=000000" alt="Close" className="w-8 h-8" /> : <img src="https://img.icons8.com/?size=100&id=hByk2bcP4aZ5&format=png&color=000000" alt="Contact" className="w-8 h-8" />}
       </button>
     </div>
   );
